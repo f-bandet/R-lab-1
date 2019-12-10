bfece7052fc42f8d84046d7de47d500542dacee5
Faye Bandet <42787446+f-bandet@users.noreply.github.com>
2019-12-09 21:55:51 -0700

    Add files via upload

---
+++ lab 1 revised.R
@@ -0,0 +1,53 @@
+# Name: Faye Bandet
+# Date: 8/29/19
+# ISTA 116 Section B || Section Leader : Jacob Heller
+# Lab Assignment 1
+# Collaborator(s): Cameron Pond and Yonny Sohn
+
+#1
+2 + 3
+# [1] 5
+
+#2
+3+4
+# [1] 7
+4+5
+# [1] 9
+5+6
+# [1] 11
+
+#3
+# 2+3
+# nothing
+
+#4
+2+3 #
+# [1] 5
+
+#5
+456#+3
+# nothing
+
+#6
+textbooks
+# [1] Error: object 'textbooks' not found
+
+#7
+library()
+# [1] Packages in library appear
+
+#8
+library(openintro)
+# [1] Prints Attaching package: ‘openintro’, cars, trees masked from ‘package:datasets’
+textbooks
+# [1] prints large data set of textbooks
+
+#9
+TEXTbooks
+# [1] Error: object 'TEXTbooks' not found
+
+#10
+plot(x=textbooks$uclaNew, y=textbooks$amazNew)
+# [1] plot(x=textbooks$uclaNew, y=textbooks$amazNew), plot appeared in right corner
+
+# Question #10 tells me the graph function will appear in the right corner of the Rstudio screen.
